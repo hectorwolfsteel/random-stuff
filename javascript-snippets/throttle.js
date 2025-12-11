5b14dfd677cf1156348ff8d344e4e5234d75a5b4
hectorwolfsteel <hectorwolfsteel@proton.me>
2025-12-11 10:00:00 +0100

    some js stuff

---
+++ javascript-snippets/throttle.js
@@ -0,0 +1,10 @@
+function throttle(fn, limit = 300) {
+  let inThrottle = false;
+  return (...args) => {
+    if (!inThrottle) {
+      fn.apply(this, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
